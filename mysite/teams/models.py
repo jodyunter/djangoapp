63631b01071d36b41c2c1b00451575bdd094e9c1
jodyu <adm_junterschutz@tcpl.ca>
2019-04-29 15:13:36 -0600

    First

---
+++ mysite/teams/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+    level = models.IntegerField(default=0)
+    wins = models.IntegerField(default=0)
+    loses = models.IntegerField(default=0)
+    ties = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "%s %d %d %d %d" % (self.name, self.level, self.wins, self.loses, self.ties)
+
+class Player(models.Model):
+    team = models.ForeignKey(Team,null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    skill = models.IntegerField(default=0)
+    goals = models.IntegerField(default=0)
+    assists = models.IntegerField(default=0)    
+
+    def __str__(self):
+        return "%s %s %d %d %d" % (self.team.name, self.name, self.skill,  self.goals, self.assists)
+
+class Game(models.Model):
+    homeTeam = models.ForeignKey(Team, related_name='homeTeam', on_delete=models.CASCADE)
+    awayTeam = models.ForeignKey(Team, related_name='awayTeam', on_delete=models.CASCADE)
+    year = models.IntegerField()
+    day = models.IntegerField()    
+    homeScore  = models.IntegerField(default=0)
+    awayScore = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "%d-%d %s %d - %d %s" % (self.year, self.day, self.homeTeam.name, self.homeScore, self.awayScore, self.awayTeam.name)
+    
